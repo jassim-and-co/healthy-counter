16d48380c080a3728107f2b0a12e86a0a4681957
jassim <jassim@fedora.server>
2018-05-22 08:56:36 +0300

    commit

--- src/features/Playground/index.js
+++ src/features/Playground/index.js
@@ -1,21 +1,5 @@
 import React from "react";
 import { runTestFunction } from "./helpers";
 
-export default () => <div class = "container">{runTestFunction()}</div>;
+export default () => <div>{runTestFunction()}</div>;
 
-// export default compose(
-//     withState("valueFrom", "setValueFrom", 0),
-//     withState("valueTo", "setValueTo", 0)
-//   )(({ valueFrom, valueTo, setValueFrom, setValueTo }) => (
-//     <div style={{ padding: "40px" }}>
-//       Value from
-//       <br />
-//       <input onChange={e => setValueFrom(e.target.value)} />
-//       <br />
-//       Value to
-//       <br />
-//       <input onChange={e => setValueTo(e.target.value)} />
-//       <div>Result</div>
-//       {getCost(window.parseInt(valueTo), window.parseInt(valueFrom))}
-//     </div>
-//   ));
