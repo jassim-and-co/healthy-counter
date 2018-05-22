9ad44ee6d28d79e17cc4c42690c3e84ef1afa4ab
jassim <jassim@fedora.server>
2018-05-22 08:36:41 +0300

    change arguments in function runTestFunction

--- src/features/Playground/helpers.js
+++ src/features/Playground/helpers.js
@@ -1,5 +1,19 @@
-function runTestFunction() {
-  return "abc";
-}
+function runTestFunction (argument1, argument2){
+  var result = 0;
+  var message = " ";
+  var symbolength1 = 0;
+  var typeOfArguments = typeof(argument1, argument2);
+   if ((argument1 === 0|argument1 === null|argument1 === undefined|typeOfArguments==="string")
+      |(argument2 === 0|argument2 === null|argument2 === undefined)){
+          var message = "Input values in incorrect, result: "}
+        else if ((argument1 <100 && argument2 < 100)){
+            var result = argument1 * argument2; var message = "result of multiply:"; 
+           } else if (argument1 >= 100 | argument2 >= 100){
+                  var result = argument1 + argument2; var message = "Result of sum: ";
+                   }
+                       
+                      return  message + result;
+                  } 
 
+console.log (container(2, 102 ));
 export { runTestFunction };
