91a4a06edd39a3f84cdd09229a512ae1747cd5cc
jassim <jassim@fedora.server>
2018-05-28 14:45:20 +0300

    commit

--- src/features/Contacts/index.js
+++ src/features/Contacts/index.js
@@ -1,6 +1,9 @@
+import PrintObject from "../../components/PrintObject";
+
 import React from "react";
 var db = {
-  cup: 0
+  cup: 0,
+  dbName: "Espresso"
 };
 
 function click() {
@@ -27,18 +30,20 @@ export default () => (
       <p>
         <button onClick={click}> EspressoCup </button>
       </p>
+      <h3>ShowCups</h3>
       <p>
-        <h3>ShowCups</h3>
         <button onClick={showData}> ShowCups </button>
       </p>
+      <h3>Massage</h3>
       <p>
-        <h3>Massage</h3>
         <button onClick={msg}> Message </button>
       </p>
+      <h3>Reset</h3>
       <p>
-        <h3>Reset</h3>
         <button onClick={rst}>Reset </button>
       </p>
+      <h3>Db</h3>
+      <PrintObject data={db} />
     </div>
   </div>
 );
