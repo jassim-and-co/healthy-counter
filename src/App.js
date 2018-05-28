045eb899ebac79dc4f4935130a4b861a1f5c9354
Rohov <rogov.dmitry@gmail.com>
2018-05-28 14:45:50 +0300

    🎨

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import Contacts from "./features/Contacts";
 import EnergyPaymentForm from "./features/EnergyPaymentForm";
 import FamiliList from "./features/FamilyList";
+import { Grid } from "material-ui";
 import MineralCounter from "./features/MineralCounter";
 import Playground from "./features/Playground";
 import create from "history/createBrowserHistory";
@@ -15,37 +16,40 @@ class App extends Component {
     return (
       <div>
         <Router history={history}>
-          <div>
-            <ul>
-              <li>
-                <Link to={"/"}>/</Link>
-              </li>
-              <li>
-                <Link to={"/healthy-counter"}>Healthy counter</Link>
-              </li>
-              <li>
-                <Link to={"/family-list"}>Simple Form</Link>
-              </li>
-              <li>
-                <Link to={"/energy-payment-form"}>Energy Payment Form</Link>
-              </li>
-              <li>
-                <Link to={"/playground"}>Playground</Link>
-              </li>
-              <li>
-                <Link to={"/contacts"}>Contacts</Link>
-              </li>
-            </ul>
-
-            <Route path={"/healthy-counter"} component={MineralCounter} />
-            <Route path={"/playground"} component={Playground} />
-            <Route path={"/family-list"} component={FamiliList} />
-            <Route path={"/contacts"} component={Contacts} />
-            <Route
-              path={"/energy-payment-form"}
-              component={EnergyPaymentForm}
-            />
-          </div>
+          <Grid container>
+            <Grid item xs={12} md={4}>
+              <ul>
+                <li>
+                  <Link to={"/"}>/</Link>
+                </li>
+                <li>
+                  <Link to={"/healthy-counter"}>Healthy counter</Link>
+                </li>
+                <li>
+                  <Link to={"/family-list"}>Simple Form</Link>
+                </li>
+                <li>
+                  <Link to={"/energy-payment-form"}>Energy Payment Form</Link>
+                </li>
+                <li>
+                  <Link to={"/playground"}>Playground</Link>
+                </li>
+                <li>
+                  <Link to={"/contacts"}>Contacts</Link>
+                </li>
+              </ul>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Route path={"/healthy-counter"} component={MineralCounter} />
+              <Route path={"/playground"} component={Playground} />
+              <Route path={"/family-list"} component={FamiliList} />
+              <Route path={"/contacts"} component={Contacts} />
+              <Route
+                path={"/energy-payment-form"}
+                component={EnergyPaymentForm}
+              />
+            </Grid>
+          </Grid>
         </Router>
       </div>
     );
